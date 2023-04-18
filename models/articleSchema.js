e433af353456fe460ab73f29f1579eae07ca3803
Eman Yassin <eman.yassin83@gmail.com>
2023-04-18 09:40:56 +0200

    updated articleSchema

---
+++ models/articleSchema.js
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+const articleSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+
+    author: {
+        type: String,
+        required: true,
+    },
+
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Comment",
+        },
+
+    ]
+
+  
+    
+},
+
+{timestamps: true},
+
+);
