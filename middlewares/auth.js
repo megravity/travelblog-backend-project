247996efa5bc7cd92e9d8e27e680302ffe6d360f
Eman Yassin <eman.yassin83@gmail.com>
2023-04-18 14:29:07 +0200

    updated auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,1 +1,27 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
+i
+
+// import UserCollection from '../models/userSchema.js';
+
+// export const auth = async (req, res, next) => {
+
+//     try{
+//         const token = req.header.token;
+
+//         const payload = jwt.verify(token, process.env.SIGNATURE);
+
+//         const user = await UserCollection.findById(payload.id);
+
+//         if(!user) {
+//             return res.status(401).json({success: false, data: "unauthorized"});
+//         }
+
+//         req.user = user;
+
+//     }catch (error) {
+
+//         res.json({success: false, data: error.message})
+//     }
+// }
+
+
